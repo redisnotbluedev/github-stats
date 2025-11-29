878e0a64db7d589a4124822dd539ed458e8aec73
redisnotbluedev <ededmaude@gmail.com>
2025-11-29 18:17:51 +0800

    add PROPER 404 (its still not proper)

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def root():
+@app.errorhandler(404)
+def not_found(error):
     return "<h1>404</h1>", 404
 
 if __name__ == "__main__":
